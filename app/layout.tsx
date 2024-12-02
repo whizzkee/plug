f787ecf59b827bbe5ae0b72b1e076121e0dce073
Trevor Johnson <75464105+tJohnsonAce@users.noreply.github.com>
2024-12-02 07:55:55 -0500

    updates to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,26 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "The Plug Coin",
   description: "Official website of Plug on Solana",
+  openGraph: {
+    title: "The Plug Coin",
+    description: "Official website of Plug on Solana",
+    images: [
+      {
+        url: "/images/plugShare.webp",
+        width: 1200,
+        height: 630,
+        alt: "The Plug Coin",
+      },
+    ],
+    type: "website",
+    url: "https://theplugcoin.com",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "The Plug Coin",
+    description: "Official website of Plug on Solana",
+    images: ["/images/plugShare.webp"],
+  },
 };
 
 export default function RootLayout({
